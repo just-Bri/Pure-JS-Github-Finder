035e9ed01d460f2699eabd56ab2c1f48311e36bd
Bridger <bridgerrhammond@gmail.com>
2019-09-16 14:28:34 -0500

    Update github.js

--- github.js
+++ github.js
@@ -9,7 +9,7 @@ class GitHub {
 
   async getUser(user) {
     const profileResponse = await fetch(
-      `http://api.github.com/users/${user}?client_id=${
+      `https://api.github.com/users/${user}?client_id=${
         this.client_id
       }&client_secret=${this.client_secret}`
     );
